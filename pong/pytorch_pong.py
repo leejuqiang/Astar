e0cd7824a8d0cd03af17c1702cbde34d446f9285
jin-sj <sj_jin@outlook.com>
2019-05-01 03:59:41 -0700

    changing to dqn, not policy net gradient

--- pong/pytorch_pong.py
+++ pong/pytorch_pong.py
@@ -1,11 +1,11 @@
 # https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
 
-import gym
 import math
 import random
 
+import gym
+
 import numpy as np
-import matplotlib.pyplot as plt
 
 import torch
 import torch.nn as nn
@@ -13,32 +13,34 @@ import torch.optim as optim
 import torch.nn.functional as F
 import torchvision.transforms as T
 from torch.autograd import Variable
-from utils.util import preprocess_observations
+from utils.image_processing import preprocess_observation
 from torch.utils.data import Dataset, DataLoader
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 UP_ACTION = 2
 DOWN_ACTION = 3
 
-class GameStateData(Dataset):
-    def __init__(self, observations, rewards):
-        #super(GameStateData).__init__()
-        self.observations = observations
-        self.rewards = rewards
+class ReplayMemory(object):
+    def __init__(self, memory_capacity):
+        self.memory = []
+        self.position = 0
 
-    def __getitem__(self, index):
-        observation = torch.tensor(self.observations[index], dtype=torch.float32)
-        reward = torch.tensor(self.rewards[index], dtype=torch.int64)
-        #observation = self.observations[index]
-        #reward = self.rewards[index]
-        return observation, reward
+    def push(self, state, action, next_state, reward):
+        data = [state, action, next_state, reward]
+        if len(self.memory) < self.memory_capacity:
+            self.memory.append(data)
+        self.memory[self.position] = data
+        self.position = (self.position + 1) % self.memory_capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
 
     def __len__(self):
-        return len(self.observations)
+        return len(self.memory)
 
 class PixelDQN(torch.nn.Module):
-    def __init__(self, num_classes):
-        super(DQN, self).__init__()
+    def __init__(self, w, h, num_classes):
+        super(PixelDQN, self).__init__()
         self.conv1 = nn.Conv2d(1, 16, kernel_size=5, stride=2)
         self.bn1 = nn.BatchNorm2d(16)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
@@ -46,10 +48,15 @@ class PixelDQN(torch.nn.Module):
         self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
         self.bn3 = nn.BatchNorm2d(32)
 
+        self.optimizer = optim.RMSprop(self.parameters(), lr=ALPHA)
+        self.loss = nn.MSELoss()
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
         # Number of Linear input connections depends on output of conv2d layers
         # and therefore the input image size, so compute it.
-        def conv2d_size_out(size, kernel_size = 5, stride = 2):
-            return (size - (kernel_size - 1) - 1) // stride  + 1
+        def conv2d_size_out(size, kernel_size=5, stride=2):
+            return (size - (kernel_size - 1) - 1) // stride + 1
         convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
         convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
         linear_input_size = convw * convh * 32
@@ -57,42 +64,61 @@ class PixelDQN(torch.nn.Module):
 
     # Called with either one element to determine next action, or a batch
     # during optimization. Returns tensor([[left0exp,right0exp]...]).
-    def forward(self, x):
+    def forward(self, observation):
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
         return self.head(x.view(x.size(0), -1))
 
-def train(dataloader, model):
-    criterion = nn.CrossEntropyLoss()
-    #criterion = nn.MultiLabelSoftMarginLoss()
+class Agent(object):
+    def __init__(self, policy_net, target_net):
+        self.policy_net = policy_net
+        self.target_net = target_net
+        self.update_count = 0
 
-    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-    for epoch in range(1):
-        running_loss = 0.0
-        for i, (data, label) in enumerate(dataloader):
-            optimizer.zero_grad()
-            data = Variable(data.view(-1, 128))
-            label = Variable(label)
+    """ Returns an action to be performed, either by random or prediction from the
+        network
+        :param model: Model representing the Q
+        :param processed_observation: Current state of the game
+        :param random_explore: Probability to return a random move
+        :return: Action to perform
+    """
+    def get_action(self, state, epsilon=0.5, random_exploration=0.2):
+        rand_prob = random.random()
+        if random_explore == 1 or rand_prob < epsilon:
+            return random.randint(UP_ACTION, DOWN_ACTION)
+        return self.policy_net(state)
 
-            output = model(data)
-            loss = criterion(output, label)
-            loss.backward()
-            optimizer.step()
+    def optimize(self, memory):
+        if len(memory) < BATCH_SIZE:
+            return
 
-""" Returns an action to be performed, either by random or prediction from the
-    network
-    :param model: Model representing the Q
-    :param processed_observation: Current state of the game
-    :param random_explore: Probability to return a random move
-    :return: Action to perform
-"""
-def get_action(model, processed_observation, random_explore=0.2):
-    rand_prob = random.random()
-    if rand_prob < random_explore:
-        return random.randint(UP_ACTION, DOWN_ACTION)
-    else:
-        return model(processed_observation)
+        batch = memory.sample(BATCH_SIZE)
+        state_batch = batch[:, 0]
+        action_batch = batch[:, 1]
+        next_state_batch = batch[:, 2]
+        reward_batch = batch[:, 3]
+
+        optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+        for epoch in range(1):
+            running_loss = 0.0
+            for i, (data, label) in enumerate(dataloader):
+                optimizer.zero_grad()
+                data = Variable(data.view(-1, 128))
+                label = Variable(label)
+
+                output = model(data)
+                loss = criterion(output, label)
+                loss.backward()
+                optimizer.step()
+        if update_count % TARGET_UPDATE == 0:
+            self.target_net.load_state_dict(self.policy_net.state_dict())
+
+    def _update_policy_net():
+        self.policy_net = self.target_net
+
+
+
 
 """ Assigns the reward to individual observations in a given sequence of observations in
     one episode
@@ -101,51 +127,64 @@ def get_action(model, processed_observation, random_explore=0.2):
     :return: Rewards with discounted factor
 """
 def assign_reward(rewards, label, gamma=0.99):
-    rewards = [r * gamma**(len(rewards)-i) for i, x in enumerate(rewards)]
+    rewards = [label * gamma**(len(rewards)-i) for i, x in enumerate(rewards)]
     return rewards
 
 
 def main():
-    seed = 42
-    env = gym.make('Pong-v4')
-    observation = env.reset()
-    prev_processed_observation = None
-
     num_classes = 2
     h = 64
     w = 64
 
-    model = PixelDQN(num_classes)
+    seed = 42
+    env = gym.make('Pong-v4')
+    env.reset()
 
-    episode_observations = []
-    episode_rewards = []
-    number_of_games_left = 1
+    model = PixelDQN(w, h, num_classes)
+
+    num_episodes = 10
+    for i_episode in range(num_episodes):
+        current_observation = get_screen()
+        previous_observation = get_screent()
+        state = current_observation - previous_observation
+        while True:
+            action = get_action(state)
+            _, reward, done, _ = env.step(action)
+
+            previous_observation = current_observation
+            current_observation = get_screen()
+            next_state = current_observation - previous_observation
+            memory.push(state, action, next_state, reward)
+
+            state = next_state
+            optimize(memory)
+            if done:
+                next_state = None
+                break
+
+    def push(self, state, action, next_state, reward):
+
     while number_of_games_left > 0:
-        env.render()
-        processed_observations, prev_processed_observations =\
-                preprocess_observations(observation, prev_processed_observations,
-                                        h, w)
 
-        action = get_action(model, processed_observation, random_explore)
-        observation, reward, done, info = env.step(action)
-        observations.append(observation)
-        rewards.append(np.sign(reward))
+        action = get_action(model, current_observation, random_explore)
+        previous_observation = current_observation
+
+        current_observation, reward, done, info = env.step(action)
+        episode_observations.append(current_observation)
+        episode_rewards.append(np.sign(reward))
         # End of an episode (a scoring event happend)
         if reward != 0:
-            degraded_rewards = assign_reward(rewards)
-            episode_rewards = episode_rewards + degraded_rewards
-            episode_observations = episode_observations + observations
-            observations = []
-            rewards = []
-            if done:
-                data = GameStateData(episode_observations, episode_rewards)
-                dataloader = DataLoader(data)
+            discounted_rewards = assign_reward(episode_rewards)
+            data = GameStateData(episode_observations, discounted_rewards)
+            dataloader = DataLoader(data)
+            train(dataloader, model)
 
-                train(dataloader, model)
+            if done:
+                previous_observation = None
                 # clear the observations and rewards
-                episode_observations = []
-                episode_rewards = []
                 number_of_games_left -= 1
+            episode_observations = []
+            episode_rewards = []
 
 if __name__ == "__main__":
     main()
--- pong/utils/image_processing.py
+++ pong/utils/image_processing.py
@@ -15,7 +15,7 @@ def remove_background(image):
     image[image == 109] = 0
     return image
 
-def preprocess_observations(input_observation, prev_processed_observation, h, w):
+def preprocess_observation(input_observation):
     processed_observation = input_observation[35:195] # crop
     processed_observation = downsample(processed_observation)
     processed_observation = remove_color(processed_observation)
@@ -23,11 +23,9 @@ def preprocess_observations(input_observation, prev_processed_observation, h, w)
     processed_observation[processed_observation != 0] = 1 # everything else (paddles, ball) just set to 1
 
     # subtract the previous frame from the current one so we are only processing on changes in the game
-    if prev_processed_observation is not None:
-        input_observation = processed_observation - prev_processed_observation
-    else:
-        input_observation = np.zeros(h, w)
     # store the previous frame so we can subtract from it next time
-    prev_processed_observations = processed_observation
-    return input_observation, prev_processed_observations
+    return input_observation
 
+def get_screen(env):
+    observation = env.render()
+    return preprocess_observation(observation)
