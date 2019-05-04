174b20f843ac2c6b47e2673d8d3545c4e26a4775
jin-sj <sj_jin@outlook.com>
2019-05-03 23:08:05 -0700

    cuda

--- pong/pytorch_pong.py
+++ pong/pytorch_pong.py
@@ -13,8 +13,8 @@ from torch.autograd import Variable
 from utils.image_processing import get_screen
 from torch.utils.data import Dataset, DataLoader
 
-#DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-DEVICE = "cpu"
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#DEVICE = "cpu"
 ALPHA = 0.0001
 UP_ACTION = 0
 DOWN_ACTION = 1
@@ -54,14 +54,13 @@ class PixelDQN(torch.nn.Module):
         self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
         self.bn3 = nn.BatchNorm2d(32)
 
-        self.optimizer = optim.RMSprop(self.parameters(), lr=ALPHA)
-        self.device = DEVICE
-        self.to(self.device)
+        self.to(DEVICE)
 
         # Number of Linear input connections depends on output of conv2d layers
         # and therefore the input image size, so compute it.
         def conv2d_size_out(size, kernel_size=5, stride=2):
             return (size - (kernel_size - 1) - 1) // stride + 1
+
         convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
         convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
         linear_input_size = convw * convh * 32
@@ -83,12 +82,12 @@ class Agent(object):
         self.policy_net = policy_net
         self.target_net = target_net
         self.update_count = 0
+        self.g_loss = 0.
 
     """ Returns an action to be performed, either by random or prediction from the
         network
         :param model: Model representing the Q
         :param processed_observation: Current state of the game
-        :param random_explore: Probability to return a random move
         :return: Action to perform
     """
     def get_action(self, state, epsilon=0.5):
@@ -96,29 +95,31 @@ class Agent(object):
         if rand_prob < epsilon:
             return random.randint(UP_ACTION, DOWN_ACTION)
         state = np.expand_dims(state, 0)
-        return np.argmax(self.policy_net(torch.tensor(state, dtype=torch.float)).tolist())
+        return np.argmax(self.policy_net(torch.tensor(state, dtype=torch.float).to(DEVICE)).tolist())
 
     def optimize(self, memory, batch_size):
         optimizer = optim.RMSprop(self.policy_net.parameters())
         loss_func = nn.MSELoss()
         batch = memory.sample(batch_size)
         batch = list(zip(*batch)) # List of columns
-        state_batch = torch.tensor(batch[0], dtype=torch.float)
-        action_batch = torch.tensor(batch[1], dtype=torch.long)
-        next_state_batch = torch.tensor(batch[2], dtype=torch.float)
-        reward_batch = torch.tensor(batch[3], dtype=torch.float)
+        state_batch = torch.tensor(batch[0], dtype=torch.float).to(DEVICE)
+        action_batch = torch.tensor(batch[1], dtype=torch.long).to(DEVICE)
+        next_state_batch = torch.tensor(batch[2], dtype=torch.float).cuda()
+        reward_batch = torch.tensor(batch[3], dtype=torch.float).to(DEVICE)
 
-        #np.amax(a, axis=1)
         a = self.target_net(next_state_batch).tolist()
         target_rewards = np.amax(a, axis=1) * GAMMA + reward_batch.tolist()
-        target_rewards = torch.tensor(target_rewards, dtype=torch.float)
+        target_rewards = torch.tensor(target_rewards, dtype=torch.float).to(DEVICE)
         policy_rewards = self.policy_net(state_batch).gather(1, action_batch)
         loss = loss_func(policy_rewards, target_rewards)
+        self.g_loss += loss
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
     def _update_target_net(self):
+        print("Loss at update: %.4f" % self.g_loss)
+        self.g_loss = 0.
         self.target_net.load_state_dict(self.policy_net.state_dict())
 
 """ Assigns the reward to individual observations in a given sequence of observations in
@@ -142,20 +143,20 @@ def main():
     env = gym.make('Pong-v4')
     env.reset()
 
-    policy_net = PixelDQN(w, h, num_classes)
-    target_net = PixelDQN(w, h, num_classes)
+    policy_net = PixelDQN(w, h, num_classes).cuda()
+    target_net = PixelDQN(w, h, num_classes).cuda()
     target_net.load_state_dict(policy_net.state_dict())
     agent = Agent(policy_net, target_net)
 
     memory = ReplayMemory(1000)
 
-    PATH = "/Users/sjjin/class/cs686/Astar/pong/weights/weight"
+    #PATH = "/Users/sjjin/class/cs686/Astar/pong/weights/weight"
+    PATH = "/home/jin/workspace/Astar/pong/weights/weight"
     train = True
     epsilon = 0.5
     frame = 1
     if train:
         for i_episode in range(1, num_episodes+1):
-            episode_reward = 0
             total_reward = 0
             print("i_episode: %d" % i_episode)
             states = []
@@ -182,15 +183,15 @@ def main():
                     agent.optimize(memory, batch_size)
                 state = next_state
                 frame += 1
-                if frame % 100 == 0:
-                    print("Frame: %d" % frame)
+                #if frame % 100 == 0:
+                #    print("Frame: %d" % frame)
                 if reward != 0:
                     total_reward = total_reward + reward
                 if frame % 1000 == 0:
                     agent._update_target_net()
 
                 if done:
-                    print("Episode: %d, total reward: %d" % (i_episode, episode_reward))
+                    print("Episode: %d, total reward: %d" % (i_episode, total_reward))
                     next_state = None
                     epsilon = np.max([0.2, epsilon * 0.99])
                     break
