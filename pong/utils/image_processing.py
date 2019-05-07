323486cae470337d6b9ed29360b3a477e9038610
jin-sj <sj_jin@outlook.com>
2019-05-07 02:18:07 -0700

    bigger value for relevant pixels

--- pong/utils/image_processing.py
+++ pong/utils/image_processing.py
@@ -1,4 +1,5 @@
 import numpy as np
+from PIL import Image
 
 def downsample(image):
     # Take only alternate pixels - basically halves the resolution of the image (which is fine for us)
@@ -14,11 +15,12 @@ def remove_background(image):
     return image
 
 def preprocess_observation(input_observation):
+    #im0 = Image.fromarray(input_observation)
     processed_observation = remove_color(input_observation)
     processed_observation = processed_observation[35:195] # crop
     processed_observation = downsample(processed_observation)
     processed_observation = remove_background(processed_observation)
-    processed_observation[processed_observation != 0] = 1 # everything else (paddles, ball) just set to 1
+    processed_observation[processed_observation != 0] = 100 # everything else (paddles, ball) just set to 1
     # (80, 80)
     # subtract the previous frame from the current one so we are only processing on changes in the game
     # store the previous frame so we can subtract from it next time
